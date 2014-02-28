b2964edd4749122217647282da90d3ce3809f112
Roland Ganaus <r.ganaus@gmail.com>
2014-02-28 19:40:27 +0100

    merged

--- Binary3DSearchTree/src/datastructures/Binary3DSearchTree.java
+++ Binary3DSearchTree/src/datastructures/Binary3DSearchTree.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package datastructures;
 
@@ -72,122 +71,7 @@ public class Binary3DSearchTree<E extends HasCoordinates> implements Iterable<E>
         }
     }
     
-    private class Binary3DSearchTreeIterator implements Iterator {
-        
-        private Node node;
-        
-        @Override
-        public boolean hasNext() {
-            if (this.node == null) {
-                return root != null ? true : false;
-            } else {
-                return true;
-            }           
-        }
-        
-        @Override
-        public E next() {
-            return this.node.value;
-        }
-        
-        @Override
-        public void remove() {
-            
-        }
-    }
-    
-    public boolean add(E e) {
-        if (root == null) {
-            root = new Node(e, null);
-        } else {
-            root.add(e, 'x');
-        }
-        
-        return true;
-    }
-    
-    public void clear() {
-        
-    }
-    
-    public Iterator iterator() {
-        return new Binary3DSearchTreeIterator();
-    }
-    
-}
-=======
-
-package datastructures;
-
-import game.HasCoordinates;
-import java.util.Iterator;
-
-/**
- *
- * @author Roland
- */
-public class Binary3DSearchTree<E extends HasCoordinates> implements Iterable<E> {
-    
-    private Node root;
-
-    private class Node {
-        private E value;
-        private Node left;
-        private Node right;
-        private int depth;
-        
-        private Node(E e) {
-            this.value = e;
-        }
-        
-        private void add(E e, char lastCriteria) {
-            if (lastCriteria == 'x') {
-                if (e.getCoordinates().getY() > this.value.getCoordinates().getY()) {
-                    if (this.right == null) {
-                        this.right = new Node(e);
-                    } else {
-                        this.right.add(e, 'y');
-                    }
-                } else {
-                    if (this.left == null) {
-                        this.left = new Node(e);
-                    } else {
-                        this.left.add(e, 'y');
-                    }
-                }
-            } else if (lastCriteria == 'y') {
-                if (e.getCoordinates().getZ() > this.value.getCoordinates().getZ()) {
-                    if (this.right == null) {
-                        this.right = new Node(e);
-                    } else {
-                        this.right.add(e, 'z');
-                    }
-                } else {
-                    if (this.left == null) {
-                        this.left = new Node(e);
-                    } else {
-                        this.left.add(e, 'z');
-                    }
-                }
-            } else {
-                if (e.getCoordinates().getX()> this.value.getCoordinates().getX()) {
-                    if (this.right == null) {
-                        this.right = new Node(e);
-                    } else {
-                        this.right.add(e, 'x');
-                    }
-                } else {
-                    if (this.left == null) {
-                        this.left = new Node(e);
-                    } else {
-                        this.left.add(e, 'x');
-                    }
-                }
-            }
-        }
-    }
-    
-    private class Binary3DSearchTreeIterator implements Iterator {
+private class Binary3DSearchTreeIterator implements Iterator {
         
         private Node last;
         
@@ -216,7 +100,7 @@ public class Binary3DSearchTree<E extends HasCoordinates> implements Iterable<E>
     
     public boolean add(E e) {
         if (root == null) {
-            root = new Node(e);
+            root = new Node(e, null);
         } else {
             root.add(e, 'x');
         }
@@ -233,4 +117,3 @@ public class Binary3DSearchTree<E extends HasCoordinates> implements Iterable<E>
     }
     
 }
->>>>>>> branch 'master' of https://github.com/RolandGanaus/Binary3DSearchTree.git
